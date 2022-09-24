808add6ab7dda7e3c418297c2507e38c79437d14
Janyson Marques <janyson29@hotmail.com>
2022-09-24 16:48:51 -0300

    ConsoleEVariáveis

---
+++ Básico/Variáveis com let/index.js
@@ -0,0 +1,23 @@
+let nome = 'João'; //String
+
+console.log(nome, 'nasceu em 1884.');
+console.log('Em 2000 João conheceu Maria.');
+console.log('João casou-se com Maria em 2012.');
+console.log('Maria teve o primeiro filho com', nome,'em 2015.');
+console.log('O filho de', nome, 'se chama Eduardo.');
+
+let nomee; //Declarou a variável
+nomee = 'Qualquer nome'; //Inicializando a variável 
+console.log(nomee);
+nomee = 'João';
+console.log(nomee);
+
+//Não pode criar variáveis com palavras reservadas do JavaScript
+//Variáveis precisam ter nomes significativos 
+//Não pode começar o nome de uma variável com um número
+//Não podem ter espaçou ou traços em variáveis 
+//Utilizamos camelCase
+//Case-sensitive
+//Não podemos redeclarar variáveis com let 
+// NÃO UTILIZE VAR, SEMPRE LET 
+
