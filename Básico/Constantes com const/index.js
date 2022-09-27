b292a2ce54c15e7848de1650d8a28a813db0913a
Janyson Marques <janyson29@hotmail.com>
2022-09-26 22:20:22 -0300

    ConstantesComConst

---
+++ Básico/Constantes com const/index.js
@@ -0,0 +1,19 @@
+//Não pode criar Constante com palavras reservadas do JavaScript
+//Constante precisam ter nomes significativos 
+//Não pode começar o nome de uma constante com um número
+//Não podem ter espaçou ou traços em variáveis 
+//Utilizamos camelCase
+//Case-sensitive
+//Não pode modificar o valor de uma constante
+// NÃO UTILIZE VAR, UTILIZE CONST. 
+const nome = 'João';
+console.log(nome);
+
+const primeiroNumero = 5;
+const segundoNumero = 10;
+const resultado = primeiroNumero * segundoNumero;
+const resultadoDuplicado = resultado * 2;
+console.log(resultadoDuplicado);
+
+console.log(typeof primeiroNumero);
+console.log(typeof nome);
