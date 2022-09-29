151a3c178229b5340a26e602933dbd2d42614c89
Janyson Marques <janyson29@hotmail.com>
2022-09-29 11:32:12 -0300

    Operadores

---
+++ Básico/Operadores aritméticos, de atribuição e incremento/index.js
@@ -0,0 +1,28 @@
+// Aritmético 
+// + Adição / Concatenação
+const num1 = 15;
+const num2 = 5;
+console.log(num1 + num2); // --> Adição
+
+// const num1 = '5'; --> Concatenação
+
+// - Subtração
+
+console.log(num1 - num2); // --> Subtração 
+
+// * Multiplicação
+
+console.log(num1 * num2); // --> Multiplicação
+
+// / Divisão
+
+console.log(num1 / num2); // --> Divisão
+
+// ** Exponenciação / Potenciação
+
+console.log(num1 ** num2); // --> Exponenciação / Potenciação
+
+// % Resto da divisão
+
+console.log(num1 % num2); // --> Resto da divisão
+
