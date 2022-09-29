5d47a35f6461c17b77c816618713d042bba14aed
Janyson Marques <janyson29@hotmail.com>
2022-09-28 21:08:56 -0300

    varElet

---
+++ Básico/Let vs Var - Primeira diferença/index.js
@@ -0,0 +1,8 @@
+var nome = 'Janyson';
+console.log(nome);
+
+var nome = 'Marques';
+console.log(nome);
+
+//let nome = 'Janyson';  var pode redeclarar uma variável, let não
+console.log(nome);
---
+++ Básico/Tipos de dados primitivos/index.js
@@ -0,0 +1,13 @@
+const nome1 = 'Janyson'; //String
+const nome2 = "Janyson"; //String
+const nome3 = `Janyson`; //String
+
+const num1 = 10 //Number
+const num2 = 10.29 //Number
+
+let nomeAluno; //Undefined -> não aponta pra local nenhum na memória
+const sobrenomeAluno = null; //Nulo -> não aponta pra local nenhum na memória
+
+const aprovado = true; //Boolean -> true ou false (LÓGICO)
+
+console.log(typeof nome1, nome1)
