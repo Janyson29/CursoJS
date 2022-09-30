73e2131840c3e1fe1b8603256cc13b1e3da4cc59
Janyson Marques <janyson29@hotmail.com>
2022-09-30 11:24:17 -0300

    Contador

--- Básico/Operadores aritméticos, de atribuição e incremento/index.js
+++ Básico/Operadores aritméticos, de atribuição e incremento/index.js
@@ -1,28 +1,75 @@
 // Aritmético 
 // + Adição / Concatenação
-const num1 = 15;
+/*const num1 = 15;
 const num2 = 5;
-console.log(num1 + num2); // --> Adição
+console.log(num1,'+', num2,'=', num1 + num2, '--> Soma'); // --> Adição
 
 // const num1 = '5'; --> Concatenação
 
 // - Subtração
 
-console.log(num1 - num2); // --> Subtração 
+console.log(num1,'-', num2,'=',num1 - num2, '--> subtração'); // --> Subtração 
 
 // * Multiplicação
 
-console.log(num1 * num2); // --> Multiplicação
+console.log(num1,'*', num2,'=',num1 * num2, '--> Multiplicação'); // --> Multiplicação
 
 // / Divisão
 
-console.log(num1 / num2); // --> Divisão
+console.log(num1,'//', num2,'=',num1 / num2, '--> Divisão'); // --> Divisão
 
 // ** Exponenciação / Potenciação
 
-console.log(num1 ** num2); // --> Exponenciação / Potenciação
+console.log(num1,'**', num2,'=',num1 ** num2, '--> Exponenciação / Potenciação'); // --> Exponenciação / Potenciação
 
 // % Resto da divisão
 
-console.log(num1 % num2); // --> Resto da divisão
+console.log(num1,'%', num2,'=',num1 % num2, '--> Resto da divisão'); // --> Resto da divisão
 
+const num3 = 10;
+
+console.log('(', num1, '+', num2,')', '*', num3, '=', (num1 + num2)* num3);
+
+const num4 = (num1 + num2) * num3;
+console.log(`(${num1} + ${num2}) * ${num3} = ${num4}`);*/
+
+/*let contador = 1;
+contador++; // 2
+contador++; // 3
+contador++; // 4
+contador++; // 5
+contador++; // 6
+contador++; // 7
+contador++; // 8
+contador++; // 9
+console.log(contador++); // +1 aqui
+console.log(contador);
+
+console.log(--contador); // -1*/
+
+// Incremento --> ++
+// Decremendo --> --
+
+let contador = 0;
+
+contador = contador + 2;
+console.log(contador);
+contador = contador + 2;
+console.log(contador);
+contador = contador + 2;
+console.log(contador);
+contador = contador + 2;
+console.log(contador);
+contador += 2; // contador = contador + 2; serve com qualquer operador
+console.log(contador);
+contador *= 2; 
+console.log(contador);
+contador /= 2; 
+console.log(contador);
+contador **= 2; 
+console.log(contador);
+contador %= 2; 
+console.log(contador);
+
+// Operadores de atribuição 
+// **= 
