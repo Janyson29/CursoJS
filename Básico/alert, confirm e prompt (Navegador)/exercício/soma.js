2e58354908149bbca8c77cf23b504240ea3585a6
Janyson Marques <janyson29@hotmail.com>
2022-10-03 18:23:18 -0300

    Alert

---
+++ Básico/alert, confirm e prompt (Navegador)/exercício/soma.js
@@ -0,0 +1,11 @@
+/*const num1 = Number(prompt('digite um número: '));
+const num2 = Number(prompt('digite o segundo número: '));
+const resultado = num1 + num2;
+alert('a soma deu ' +resultado);*/
+
+let num1 = prompt('digite um número: ');
+let num2 = prompt('digite o segundo número: ');
+num1 = Number(num1);
+num2 = Number(num2);
+const resultado = num1 + num2;
+alert('o resultado é: ' +resultado);
---
+++ Básico/alert, confirm e prompt (Navegador)/script.js
@@ -0,0 +1,6 @@
+alert('clonando o seu cartao.');
+let confirma = confirm('okey mesmo?');
+const num1 = prompt('digite o tres numeros de tras:');
+const num2 = prompt('digite o tres numeros de tras:');
+
+console.log(num1, num2)
