50917098d83c773668f928a3783f74cc772995bb
Janyson Marques <janyson29@hotmail.com>
2022-10-05 10:54:14 -0300

    maisStrings

---
+++ Básico/Mais sobre strings/index.js
@@ -0,0 +1,19 @@
+//               01234567
+let umaString = "O rato roeu a roupa do rei de roma."; // "um \"texto\"";
+//let usaebarra = "um \\texto";
+
+console.log(umaString.toUpperCase()); // tudo maiúscula 
+console.log(umaString.toLowerCase()); // tudo minúscula 
+
+//console.log(umaString.split('r')); // dividir as palavras pelo string escolhido 
+
+//console.log(umaString.slice(2,6)); //escolher uma palavra
+
+//console.log(umaString.length); // pra saber o tamanho da string 
+
+//console.log(umaString.replace('Um', 'Outra')); //substitução de palavras
+
+/*console.log(umaString.concat(' em um lindo dia.'));
+console.log(umaString + ' em um lindo dia.');
+console.log(`${umaString} em um lindo dia.`);
+console.log(usaebarra);*/
