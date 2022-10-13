9dfe185ce90f15b09877070ebe55e6844e8ffb87
Janyson Marques <janyson29@hotmail.com>
2022-10-13 18:33:12 -0300

    ObjetoMath

---
+++ Básico/Objeto Math/index.js
@@ -0,0 +1,23 @@
+console.log(100 / 0); // Erro do JavaScript resultado = Infinity
+
+
+// let num1 = 16;
+
+// console.log(num1 ** (1/2)); // Raiz quadrada
+// console.log(num1 ** 0.5); // Raiz quadrada também
+
+// let num2 = Math.floor(num1); // Arredondamento de número pra baixo 
+// let num2 = Math.ceil(num1); // Arredondamento de um número pra cima
+// let num2 = Math.round(num1); // Arredondamento de um número para o mais próximo
+
+// console.log(Math.max(1,2,3,4,-50,-100,500,44,29,81)); // O maior número da sequência 
+// console.log(Math.min(1,2,3,4,-50,-100,500,44,29,81)); // O menor número da sequência 
+
+// const aleatorio = Math.random(); // Gera um número aleatório entre 0 e 1
+
+// const aleatorio = Math.round(Math.random() * (10 - 5) + 5); // Gerando um número arredondado entre 5 e 10
+
+// console.log(Math.PI); // Valor de PI 3.1415...
+
+// console.log(Math.pow(2, 10)); // Potenciação, um número elevado a outro
+// console.log(2 ** 10); // É a mesma coisa que de cima 
