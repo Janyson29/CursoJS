1876fd840c05c8c61e45afb57a08ae4e9f3e7af7
Janyson Marques <janyson29@hotmail.com>
2022-10-17 13:06:40 -0300

    ArraysBasico

---
+++ Básico/Arrays (Básico)/index.js
@@ -0,0 +1,46 @@
+// Indices 
+/*
+// Indices Strings    012345678910...
+        const nome = 'Janyson Silva';
+
+//Arrays           0       1        2    */
+const alunos = ['Luiz', 'Maria', 'João'];
+
+
+
+console.log(typeof alunos);
+console.log(alunos instanceof Array)
+
+// alunos.push('Luiza');
+// alunos.push('Eduardo');
+// console.log(alunos.slice(0, 3)); // Mostra os nomes do elemento 0 ao 2
+
+// delete alunos[1]; // Deleta a variável, mas o elemento continua "vazio"
+
+// console.log(removido);
+// const removido = alunos.shift(); // Adicionando o removido que é o primeiro em outra variável
+// alunos.shift(); // Remove o primeiro nome
+
+// const removido = alunos.pop(); // Adicionando o removido que é o último em outra variável
+// alunos.pop(); // Remove o último elemento 
+
+
+// alunos.unshift('Luiza'); // Adiciona no começo 
+// alunos.unshift('Fábio');
+
+
+// alunos.push('Luiza'); // Adiciona no fim
+// alunos.push('Fábio');
+// alunos.push('Luana');
+
+//alunos[alunos.length] = 'Luiza'; // Adiciona no fim
+//alunos[alunos.length] = 'Fábio'; 
+//alunos[alunos.length] = 'Luana';
+
+//alunos[0] = 'Eduardo'; // Altera
+//alunos[3] = 'Luisa'; // Adicionando
+
+
+//console.log(alunos);
+//console.log(alunos[2]);
+//console.log(alunos[0]);
