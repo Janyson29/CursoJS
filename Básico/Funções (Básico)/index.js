06c9134f11f8578b04be1f508533479eae044eb8
Janyson Marques <janyson29@hotmail.com>
2022-10-25 11:51:57 -0300

    functionBasic

---
+++ Básico/Funções (Básico)/index.js
@@ -0,0 +1,44 @@
+/*function saudacao() {
+    console.log('Bom dia!')
+}
+saudacao();*/
+
+/*function saudacao(nome) {
+    console.log(`Bom dia ${nome}!`);
+    return 123456;
+}
+const variavel = saudacao('Luiz');
+console.log(variavel);*/
+
+/*function saudacao(nome) {
+    return `Bom dia ${nome}!`;
+}
+const variavel = saudacao('Luiz');
+console.log(variavel);*/
+
+/*function soma(x, y) {
+    const resultado = x + y;
+    return resultado
+}
+console.log(soma(2, 2));*/
+
+/*function soma(x, y) {
+    const resultado = x + y;
+    return resultado
+}
+const resultado = soma(2, 2);
+console.log(resultado)*/
+
+/*const raiz = function (n) {
+    return n ** 0.5;
+};
+
+console.log(raiz(9));
+console.log(raiz(16));
+console.log(raiz(25));*/
+
+const raiz = n => n ** 0.5;  // Função arrow
+
+console.log(raiz(9));
+console.log(raiz(16));
+console.log(raiz(25));
