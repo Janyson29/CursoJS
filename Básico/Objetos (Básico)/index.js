2f5c83234cf6a2ac3bf887c8cad61b3076634cad
Janyson Marques <janyson29@hotmail.com>
2022-10-27 18:46:53 -0300

    ObjetosBasicos

---
+++ Básico/Objetos (Básico)/index.js
@@ -0,0 +1,65 @@
+/*const array = [1,2,3];
+array.push(4);
+array[0] = 'Luiz';
+console.log(array);*/
+
+/*const nome01 = 'Luiz';
+const sobrenome01 = 'Miranda';
+const idade01 = 25;
+
+const nome02 = 'Maria';
+const sobrenome02 = 'Oliveira';
+const idade02 = 55;*/
+
+/*const pessoa1 = {
+    nome: 'Luiz',
+    sobrenome: 'Miranda',
+    idade: 25,
+};
+
+const pessoa2 = {
+    nome: 'Maria',
+    sobrenome: 'Oliveira',
+    idade: 55,
+};
+
+console.log(pessoa1.nome);
+console.log(pessoa1.sobrenome);*/
+
+/*function criaPessoa(nome, sobrenome, idade){
+    return {
+        nome: nome, 
+        sobrenome: sobrenome, 
+        idade: idade
+    };
+}
+
+const pessoa1 = criaPessoa('Luiz', 'Miranda', 25);
+const pessoa2 = criaPessoa('Maria', 'Oliveira', 32);
+const pessoa3 = criaPessoa('João', 'Moreira', 55);
+const pessoa4 = criaPessoa('Junior', 'Lara', 44);
+const pessoa5 = criaPessoa('Jean', 'Otávio', 69);
+console.log(pessoa1.nome, pessoa2.nome, pessoa3.nome, pessoa4.nome, pessoa5.nome);*/
+
+const pessoa1 = {
+    nome: 'Luiz',
+    sobrenome: 'Miranda',
+    idade: 25,
+
+
+    fala(){
+        console.log(`A minha idade atual é ${this.idade}.`);
+    },
+
+    incrementaIdade(){
+        this.idade++;
+    }
+}
+
+pessoa1.fala();
+pessoa1.incrementaIdade();
+pessoa1.fala();
+pessoa1.incrementaIdade();
+pessoa1.fala();
+pessoa1.incrementaIdade();
+pessoa1.fala();
