e22ec03b9043b3cf9d18abfeb0e8f92ba8dacdab
Janyson Marques <janyson29@hotmail.com>
2022-11-01 15:19:40 -0300

    Operadores de comparação

---
+++ lógica de Programação/Operadores de comparação/index.js
@@ -0,0 +1,26 @@
+/*
+Operadores de comparação
+> maior que
+>= maior que ou igual a
+< menor que
+<= menor que ou igual a
+== igualdade (valor)
+=== igualdade estrita (valor e tipo)
+!= diferente (valor)
+!== diferente estrito (valor e tipo)
+*/ 
+
+// Irá comparar valores e retornar um Boolean como resposta à comparação
+
+console.log( 10 > 5) // true
+console.log( 10 >= 5) // true
+console.log( 10 < 5) // false
+console.log( 10 <= 5) // false
+console.log( 10 == 10) // true
+console.log( 10 == '10') // true
+console.log( 10 === 10) // true
+console.log( 10 === '10') // false
+console.log( 10 != 10) // false
+console.log( 10 != '10') // false
+console.log( 10 !== 10) // false
+console.log( 10 !== '10') // true
