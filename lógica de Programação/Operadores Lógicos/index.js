e1f79c6fbc73f8fc61e77baec1c45f6fcff7d56d
Janyson Marques <janyson29@hotmail.com>
2022-11-01 19:52:42 -0300

    Operadores Lógicos

---
+++ lógica de Programação/Operadores Lógicos/index.js
@@ -0,0 +1,19 @@
+/*
+Operadores Lógicos
+&& -> AND -> E -> Todas as expressões precisam ser verdadeiras para retornar true
+|| -> OR -> OU -> Uma das expressões precisam ser verdadeiras para retornar true
+! -> NOT -> NÃO -> Nega uma expressão
+*/
+
+console.log(true && true); // true
+console.log(true && false); // false
+console.log(true || false); // true
+console.log(false || false); // false
+console.log(!true); // false
+console.log(!false); // true
+
+/*const usuario = 'Luiz';
+const senha = '123456';
+
+const vaiLogar = usuario === 'Luiz' && senha === '123456';
+console.log(vaiLogar);*/
