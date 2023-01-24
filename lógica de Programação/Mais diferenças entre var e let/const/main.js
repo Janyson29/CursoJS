4b0d714686284a03546e64eea250c64f7989fb3f
Janyson Marques <janyson29@hotmail.com>
2023-01-24 20:46:29 -0300

    Mais diferenças entre var e let/const

---
+++ lógica de Programação/Mais diferenças entre var e let/const/main.js
@@ -0,0 +1,35 @@
+const verdadeira = true;
+
+// Let tem escopo de bloco {...bloco}
+// Var só tem escopo de função
+
+// let nome = 'Luiz'; // criando
+// var nome2 = 'Luiz'; // criando
+ 
+// if (verdadeira) {
+//   let nome = 'Otávio'; // criando
+//   var nome2 = 'Otávio'; // redeclarando
+//     console.log(nome, nome2);
+//   if (verdadeira) {
+//     let nome = 'Ricardo'; // criando
+//     var nome2 = 'Ricardo'; // redeclarando
+//     console.log(nome, nome2);
+//   }
+// }
+
+// console.log(nome, nome2);
+
+// Exemplo 2
+
+// function falaOi () {
+//     if (verdadeira) {
+//         let nome = 'Luiz';
+//         var sobrenome = 'Miranda';
+//         console.log(nome, sobrenome);
+//     }
+//     console.log(sobrenome);
+// }
+
+// falaOi();
+
+
