bac9f4b688330786e168d6672c348f42134f8047
Janyson Marques <janyson29@hotmail.com>
2023-01-27 12:48:02 -0300

    For - Clássico - Estrutura de repetição

---
+++ lógica de Programação/Clássico - Estrutura de repetição/index.js
@@ -0,0 +1,38 @@
+// console.log("linha 0");
+// console.log("linha 1");
+// console.log("linha 2");
+// console.log("linha 3");
+// console.log("linha 4");
+// console.log("linha 5");
+
+// i = index
+
+// for (let i = 0; i <= 5; i++) { // ++ para somar
+//     console.log(`linha ${i}`); 
+// }
+
+// for (let i = 10; i >= 1; i--) { // -- para subtrair
+//     console.log(`linha ${i}`); 
+// }
+
+// for (let i = 0; i <= 100; i += 10) { // += para adiciomar mais de 1 
+//     console.log(`linha ${i}`); 
+// }
+
+// for (let i = 100; i >= 1; i -= 10) { // -= para diminuir menos de 1 
+//     console.log(`linha ${i}`); 
+// }
+
+// for (let i = -100; i <= 100; i += 10) { // para começar de numeros negativos 
+//     console.log(`linha ${i}`); 
+// }
+
+// for (let i = 0; i <= 10; i++) {
+//     const par = i % 2 === 0 ? "par" : "impar";
+//     console.log(i,par)
+// }
+
+const frutas = ['Maça', 'Banana', 'Uva', 'Laranja', 'Abacaxi', 'Pêra', 'Goiaba', 'Limão'];
+for (let i = 0; i < frutas.length; i++ ) {
+    console.log(`Indice ${i}`, frutas[i]);
+}
