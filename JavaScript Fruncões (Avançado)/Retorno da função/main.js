602a487f724548e354ae761a4bc108769a2eb910
Janyson Marques <janyson29@hotmail.com>
2023-02-28 17:25:00 -0300

    Retorno da função

---
+++ JavaScript Fruncões (Avançado)/Retorno da função/main.js
@@ -0,0 +1,62 @@
+// return
+// Retorna um valor 
+// Termina a função
+
+// function soma(a, b) {
+//     return a + b;
+// }
+
+// document.addEventListener('click', function() {
+//     document.querySelector.button.backgroundColor = 'black'
+// })
+
+// function criaPessoa(nome, sobrenome){
+//     return { nome, sobrenome };
+// }
+
+// const p1 = criaPessoa('luiz', 'otavio');
+// const p2 = {
+//     nome: 'lucas',
+//     sobrenome: 'silva'
+// }
+
+// console.log(typeof p1);
+// console.log(typeof p2);
+
+// function falaFrase(comeco) {
+//     function falaResto(resto) {
+//         return comeco + ' ' + resto;
+//     }
+//     return falaResto
+// }
+
+// const fala = falaFrase('Olá');
+// const resto = fala('mundo!');
+
+// console.log(resto);
+
+// function duplica(n) {
+//     return n * 2;
+// }
+
+// function triplica(n) {
+//     return n * 3
+// }
+
+// function quadriplica(n) {
+//     return n * 4
+// }
+
+function criaMultiplicador(multiplicador) {
+    return function(n) {
+        return n * multiplicador;
+    };
+};
+
+const duplica = criaMultiplicador(2);
+const triplica = criaMultiplicador(3);
+const quadriplica = criaMultiplicador(4);
+
+console.log(duplica(2));
+console.log(triplica(2));
+console.log(quadriplica(2));
