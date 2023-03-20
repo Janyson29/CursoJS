e7680b1185ea4bd56a7a87d41ac9a5deaf83864b
Janyson Marques <janyson29@hotmail.com>
2023-03-20 18:40:59 -0300

    Método Splice

---
+++ JavaScript Arrays (Avançado)/Método Splice/index.js
@@ -0,0 +1,21 @@
+//               -5      -4        -3         -2        -1
+//                0       1         2          3         4 
+const nomes = ['Maria', 'João', 'Eduardo', 'Gabriel', 'Júlia'];
+
+// nomes.splice(indice, delete, eleme1, eleme2, eleme3);
+
+// pop
+// const removidos = nomes.splice(3, 1);
+
+// shift
+// const removidos = nomes.splice(0, 1);
+
+// console.log(nomes, removidos);
+
+// Push
+// nomes.splice(nomes.length, 0, 'Luiz');
+
+
+// unshitf 
+nomes.splice(0, 0, 'Luiz', 'Otávio');
+console.log(nomes);
